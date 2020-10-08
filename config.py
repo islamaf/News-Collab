f01043aeaab508a761895a43e60bd474d0961b8f
islamaf <44944648+islamaf@users.noreply.github.com>
2020-10-08 15:38:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 # general app config
 import os
-secret_key = os.urandom(12) #Secret validation key
+
+secret_key = os.urandom(12)  # Secret validation key
+users_db_path = 'credentials.db'
+posts_db_path = 'interaction.db'
