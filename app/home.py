dfd91ef319dccb2da46d1f4c1415092d3812a72d
islamaf <44944648+islamaf@users.noreply.github.com>
2020-10-08 15:35:52 +0300

    Update home.py

--- app/home.py
+++ app/home.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
-home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_folder='static')
+home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_folder='static', static_url_path='/static/home_bp')
 
 @home_bp.route('/')
 def home():
+    return render_template('splash.html')
+    # return bbc_sport_parsing()
+
+@home_bp.route('/home')
+def home2():
+    if session['logged_in']:
+        session['logged_in'] = True
+        username = session['username'].capitalize()
+        return render_template('home.html', username=username)
+    session['logged_in'] = False
     return render_template('home.html')
 
