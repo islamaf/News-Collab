a15c2dea6876e5543c99e27394d61532bc1b8c9a
islamaf <44944648+islamaf@users.noreply.github.com>
2020-10-08 15:40:09 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,15 @@ from flask import Flask
 from config import secret_key
 
 from app.home import home_bp
+from app.login import login_bp
+from app.leParser import parser_bp
+from app.userProfile import user_bp
 
 app = Flask(__name__)
 app.register_blueprint(home_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(parser_bp)
+app.register_blueprint(user_bp)
 
 if __name__ == "__main__":
     app.secret_key = secret_key
